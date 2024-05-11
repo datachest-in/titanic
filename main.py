a177ba772bf88565debfcf626479bf64f5d80fd5
DataChest <abhisek.sdev@gmail.com>
2024-05-11 11:41:30 +0530

    finding datafile information finding datafile information

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os
 from jproperties import Properties
 
 from utils.file_info import check_for_files
+from utils.file_info import get_data_file_info
 
 
 # Reading .properties file.
@@ -18,6 +19,7 @@ with open('main_config.properties', 'rb') as cfg_file:
     config.load(cfg_file)
 # Get all config values from  .properties file.
 data_dir = config.get('data_dir').data
+data_file = config.get('data_file').data
 # Closing the config file.
 cfg_file.close()
 
@@ -33,9 +35,16 @@ print("\nRoot Directory: ",ROOT_DIR)
 DATA_DIR = os.path.join(ROOT_DIR, data_dir)
 print("\nData Source Directory: ",DATA_DIR)
 
+# Data Analysis
+DATA_FILE = os.path.join(DATA_DIR, data_file)
+
+
+
+
 
 # Main code
 if __name__ == '__main__':
     check_for_files(DATA_DIR)
+    get_data_file_info(DATA_FILE)
 
 
--- utils/file_info.py
+++ utils/file_info.py
@@ -8,11 +8,14 @@
 # Import modules
 import os
 
+from datetime import datetime as dt
+from tzlocal import get_localzone
+
 
 def check_for_files(directory):
     """
     Check for files in the directory.
-    
+
     Args:
     - directory (str): Path to the directory.
     """
@@ -32,23 +35,20 @@ def check_for_files(directory):
 def list_file_extensions_with_counts(directory):
     """
     List all file extensions present in a directory along with the number of files for each extension.
-    
+
     Args:
     - directory (str): Path to the directory.
     """
     # Initialize a dictionary to store file extension counts
     extension_counts = {}
-
     # Iterate through the files in the directory
     for filename in os.listdir(directory):
         # Split the filename into its base and extension
         _, ext = os.path.splitext(filename)
         # Increment the count for this extension in the dictionary
         extension_counts[ext] = extension_counts.get(ext, 0) + 1
-    
     # Sort the file extensions alphabetically
     sorted_extensions = sorted(extension_counts.items(), key=lambda x: x[0])
-
     # Print the number of files for each extension
     print("\nFile types and their counts:")
     for ext, count in sorted_extensions:
@@ -64,17 +64,45 @@ def list_files_sorted_by_extension(directory):
     """
     # Initialize a list to store file names
     files = []
-
     # Iterate through the files in the directory
     for filename in os.listdir(directory):
         # Append the filename to the list
         files.append(filename)
-
     # Sort the files alphabetically by their extensions
     files.sort(key=lambda x: (os.path.splitext(x)[1], x))
-
     # Print the sorted files
     print("\nFiles available in datasource directory:")
     print("________________________________________")
     for file in files:
         print(file)
+
+
+def get_data_file_info(filename):
+    print("\nData file to be analysed: ", filename)
+    filetype = os.path.splitext(filename)[1]
+    print("File type: ", filetype)
+    filesize = os.path.getsize(filename)
+    print("File size (bytes): ", filesize)
+    file_modified_time = get_last_modified_time(filename)
+    print("Time of last modification (UTC):", file_modified_time, sep="\t")
+
+
+def get_last_modified_time(file_path):
+    """
+    Get the last modified time of a file in the local timezone.
+    
+    Args:
+    - file_path (str): Path to the file.
+    
+    Returns:
+    - str: Last modified time of the file in the local timezone, formatted as a string.
+    """
+    # Get the last modified time of the file in seconds since the epoch
+    last_modified_time = os.path.getmtime(file_path)
+    # Get the local timezone
+    local_timezone = get_localzone()
+    # Convert the time to a datetime object and localize it to the local timezone
+    localized_last_modified_datetime = dt.fromtimestamp(last_modified_time).replace(tzinfo=local_timezone)
+    # Format the localized datetime as a string
+    formatted_time = localized_last_modified_datetime.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    return formatted_time
