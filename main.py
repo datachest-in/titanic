916dd091ebcf11b619d4eb9544be28022ddf21de
DataChest <abhisek.sdev@gmail.com>
2024-05-11 14:20:49 +0530

    Checked for the datafile and covert to dataframe for further analysis.

--- main.py
+++ main.py
@@ -7,7 +7,9 @@
 
 # Import modules
 import os
+import pandas as pd
 from jproperties import Properties
+
 
 from utils.file_info import check_for_files
 from utils.file_info import get_data_file_info
@@ -38,7 +40,15 @@ print("\nData Source Directory: ",DATA_DIR)
 # Data Analysis
 DATA_FILE = os.path.join(DATA_DIR, data_file)
 
-
+df = pd.read_csv(DATA_FILE)
+print(df)
+# Extracting the column names from the data file.
+columns=df.columns.to_list()
+print("\nColumn Names: ", ", ".join(columns))
+cols=df.shape[1] # number of columns in the data file.
+print("\nNumber of Columns: ", cols)
+rows=df.shape[0] # number of rows in the data file.
+print("Number of Rows: ", rows)
 
 
 
